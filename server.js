b7145de86026d1425d38fccb8e890f310d1cd5b5
Dmitriy Klimovich <klimovichdr@gmail.com>
2015-06-05 11:04:20 +0600

    commit

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-var express = require("express"),
-    mongoose = require("mongoose"),
-    passport = require("passport"),
-    LocalStrategy = require("passport-local").Strategy;
+var express = require("express");
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
-
 
 var config = require("./server/config/config")[env];
 
@@ -14,50 +10,10 @@ require("./server/config/express")(app, config);
 
 require("./server/config/mongoose")(config);
 
-var User = mongoose.model('User');
-passport.use(new LocalStrategy(function (userName, password, done) {
-    User.findOne({UserName:userName}).exec(function (err, user) {
-        if (user) {
-            return done(null, user);
-        }
-        else {
-            return done(null, false);
-        }
-    });
-}));
-
-passport.serializeUser(function (user, done) {
-    done(null, user._id);
-});
-
-passport.deserializeUser(function (id, done) {
-    User.findById(id, function (err, user) {
-        done(err, user);
-    })
-});
-
+require("./server/config/passport")();
 
 require("./server/config/routes")(app);
 
 app.listen(config.port, config.ip);
 console.log('Listening ip ' + config.ip + ' ...');
 console.log('Listening port ' + config.port + ' ...');
-
-
-/*app.get('/api/users', function (req, res) {
-    mongoose.model('User').find({}).exec(function (error, collection) {
-        res.send(collection);
-    });
-});
-app.get('*', function(req, res) {
-    res.render('index');
-});*/
-
-/*mongoose.connect('mongodb://localhost/mircvetovkz');
-var con = mongoose.connection;
-con.once('open', function () {
-   console.log('connected to mongodb');
-   userModel.createDefaultUsers();
-});*/
-
-
--- server/models/User.js
+++ server/models/User.js
@@ -1,26 +1,75 @@
 var mongoose = require("mongoose"),
-    Schema = mongoose.Schema;
+    Schema = mongoose.Schema,
+    encrypt = require('../utilities/encryption');
 
 var userSchema = new Schema({
-	UserName: String,
-	FirstName: String,
-	LastName: String,
-	Email: String,
-	Phone: String,
-	Roles: [String]
+    FirstName: {type: String, required: '{PATH} обязательное поле'},
+    LastName: {
+        type: String, required: '{PATH} обязательное поле'
+    },
+    UserName: {
+        type: String,
+        required: '{PATH} обязательное поле',
+        unique: true
+    },
+    Salt: {type: String, required: '{PATH} обязательное поле!'},
+    Hashed_pwd: {type: String, required: '{PATH} обязательное поле!'},
+    Email: String,
+    Phone: String,
+    Roles: [String]
 });
+
+userSchema.methods = {
+    authenticate: function (passwordToMatch) {
+        return encrypt.hashPwd(this.Salt, passwordToMatch) === this.Hashed_pwd;
+    },
+    hasRole: function (role) {
+        return this.Roles.indexOf(role) > -1;
+    }
+
+};
 
 var User = mongoose.model('User', userSchema, 'Users');
 
 function createDefaultUsers() {
-    User.find({}).exec(function (error, collection) {
+    User.find({}).exec(function (err, collection) {
         if (collection.length === 0) {
+            var salt, hash;
+
+            salt = encrypt.createSalt();
+            hash = encrypt.hashPwd(salt, 'Qaz1wsx2');
             User.create({
-                UserName: 'admin', 
                 FirstName: 'Администратор',
-                Email: 'admin@mircvetov.kz',
-                Roles: [ 'admin' ]
-                
+                LastName: 'Администратор',
+                UserName: 'admin',
+                Salt: salt,
+                Hashed_pwd: hash,
+                Email: 'admin@sitenodejs.kz',
+                Roles: ['admin']
+            });
+
+            salt = encrypt.createSalt();
+            hash = encrypt.hashPwd(salt, 'Qaz1wsx2');
+            User.create({
+                FirstName: 'Пользователь 1',
+                LastName: 'Пользователь 1',
+                UserName: 'user1',
+                Salt: salt,
+                Hashed_pwd: hash,
+                Email: 'user1@sitenodejs.kz',
+                Roles: ['user']
+            });
+
+            salt = encrypt.createSalt();
+            hash = encrypt.hashPwd(salt, 'Qaz1wsx2');
+            User.create({
+                FirstName: 'Пользователь 2',
+                LastName: 'Пользователь 2',
+                UserName: 'user2',
+                Salt: salt,
+                Hashed_pwd: hash,
+                Email: 'user2@sitenodejs.kz',
+                Roles: ['user']
             });
         }
     });
